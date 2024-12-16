1c07dc6008d3c539a85aef0fc6a1f05c77d699bd
Braun Roland <rolibraun94@gmail.com>
2024-12-16 16:36:37 +0100

    F01 feladat elkészítve

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -8,8 +8,24 @@ namespace ConsoleApp7
 {
 	internal class Program
 	{
+
+		static void F01(int[] tomb)
+		{
+			Random rnd = new Random();
+			for (int i = 0; i < tomb.Length; i++)
+			{
+				int szam = rnd.Next(1, 51);
+                Console.Write(tomb[i] + " + " + szam + " = ");
+				tomb[i] += szam;
+                Console.WriteLine(tomb[i]);
+            };
+		}
+
 		static void Main(string[] args)
 		{
+            int[] tomb = { 2, 12, 56};
+            F01(tomb);
+			Console.ReadLine();
 		}
 	}
 }
