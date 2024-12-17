673fc0ccc3a68b8756f1cee41efdf81a947974b9
Braun Roland <rolibraun94@gmail.com>
2024-12-17 08:43:00 +0100

    F02 feladat elkészítve

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -15,16 +15,30 @@ namespace ConsoleApp7
 			for (int i = 0; i < tomb.Length; i++)
 			{
 				int szam = rnd.Next(1, 51);
-                Console.Write(tomb[i] + " + " + szam + " = ");
+				Console.Write(tomb[i] + " + " + szam + " = ");
 				tomb[i] += szam;
-                Console.WriteLine(tomb[i]);
-            };
+				Console.WriteLine(tomb[i]);
+			};
+		}
+
+		static void F02(int[] tomb)
+		{
+			int szamlalo = 0;
+			for (int i = 0; i < tomb.Length; i++)
+			{
+				if (tomb[i] % 2 == 0)
+				{
+					szamlalo++;
+				}
+			}
+			Console.WriteLine($"ennyi páros szám van a tömbben: {szamlalo} ");
 		}
 
 		static void Main(string[] args)
 		{
-            int[] tomb = { 2, 12, 56};
-            F01(tomb);
+			int[] tomb = { 2, 12, 56};
+			F01(tomb); 
+			F02(tomb);
 			Console.ReadLine();
 		}
 	}
